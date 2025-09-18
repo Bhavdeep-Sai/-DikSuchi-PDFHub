3a360c77df804b99769d13972c9639fe0e963232
Bhavdeep-Sai <bhavdeepsai@gmail.com>
2025-09-18 15:48:52 +0530

    url update

--- frontend/src/api/pdfApi.js
+++ frontend/src/api/pdfApi.js
@@ -1,7 +1,7 @@
 // src/api/pdfApi.js
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:5000/api';
+const API_BASE = 'https://diksushi-pdfhub.onrender.com/api';
 
 // ====================== Public PDF APIs ======================
 
